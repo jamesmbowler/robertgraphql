f512d2d21a9c338704f11123b09247707d05876a
James Bowler <jamesbowler@Jamess-MacBook-Pro.local>
2023-04-24 15:41:01 -0700

    generation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
+
 plugins {
     `kotlin-dsl`
+    id("com.netflix.dgs.codegen") version "5.7.1"
 }
 
 repositories {
@@ -8,4 +10,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    //implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+    implementation("com.squareup:kotlinpoet:1.12.0")
+    implementation("com.netflix.graphql.dgs:graphql-dgs")
+    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-core:5.7.1")
+    implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-shared-core:5.7.1")
 }
--- src/main/kotlin/com/example/paymentsv2/datafetchers/AddressesDataFetcher.kt
+++ src/main/kotlin/com/example/paymentsv2/datafetchers/AddressesDataFetcher.kt
@@ -1,21 +1,28 @@
 package com.example.paymentsv2.datafetchers
 
+import com.example.paymentsv2.models.Address
+import com.example.paymentsv2.repositories.AddressRepository
+import com.example.paymentsv2.utils.JoinChildren
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsQuery
+import graphql.schema.DataFetchingEnvironment
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.domain.Specification
 
 @DgsComponent
 class AddressesDataFetcher {
 
-//    @Autowired
-//    lateinit var repository: AddressRepository
+    @Autowired
+    lateinit var repository: AddressRepository
 
-//    @DgsQuery
-//    fun addresses(environment: DataFetchingEnvironment): List<Address> {
-//
-//        val spec: Specification<Address>? = JoinChildren().fetchChildEntity<Address>(
-//            environment.selectionSet.immediateFields,
-//            Address::class.java
-//        )
-//
-//        return repository.findAll(spec!!)
-//    }
+    @DgsQuery
+    fun addresses(environment: DataFetchingEnvironment): List<Address> {
+
+        val spec: Specification<Address>? = JoinChildren().fetchChildEntity<Address>(
+            environment.selectionSet.immediateFields,
+            null
+        )
+
+        return repository.findAll(spec!!)
+    }
 }
