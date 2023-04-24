9311df9444d0976834e01bb36f5f0fa5ec80d899
James Bowler <jamesbowler@Jamess-MacBook-Pro.local>
2023-04-23 22:31:36 -0700

    plugin

--- buildSrc/src/main/kotlin/com/example/RobGenerator.kt
+++ buildSrc/src/main/kotlin/com/example/RobGenerator.kt
@@ -1,12 +1,12 @@
-//package com.example
-//
-//import org.gradle.api.DefaultTask
-//import org.gradle.api.tasks.TaskAction
-//
-//open class RobGenerator : DefaultTask() {
-//
-//    @TaskAction
-//    fun run() {
-//        println("hello from GreetingTask")
-//    }
-//}
+package com.example
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+open class RobGenerator : DefaultTask() {
+
+    @TaskAction
+    fun run() {
+        println("hello from GreetingTask")
+    }
+}
